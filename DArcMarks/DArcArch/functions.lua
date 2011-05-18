3337696070380d54348e2ba2d768c47bad8e032f
David Arceneaux <david@davidthemachine.org>
2011-05-18 15:46:14 -0700

    Move, rename, rereference all the directories, and base filenames

--- DArcMarks/DArcArch/functions.lua
+++
@@ -1,7 +0,0 @@
--------------------
--- Binding Labes --
--------------------
-
-_G["BINDING_HEADER_DARC_H_ARCH"]     = "Archaeology"
-_G["BINDING_NAME_SPELL Survey"]      = "Survey"
-_G["BINDING_NAME_SPELL Archaeology"] = "Archaeology Frame"
