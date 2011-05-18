150bb354b2be2bf3b9e0f87eeada6a4cdbf09808
David Arceneaux <david@davidthemachine.org>
2011-05-18 15:31:51 -0700

    bump TOC version, and fix a header reference typo, so the name shows up in the wow menu

--- DArcMarks/DArcConvenience/functions.lua
+++ DArcMarks/DArcConvenience/functions.lua
@@ -7,7 +7,7 @@ _G["BINDING_NAME_CLICK DArc_Focus:LeftButton"]     = "Focus mouseover, target, o
 _G["BINDING_NAME_CLICK DArc_Assist:LeftButton"]    = "Assist Focus"
 _G["BINDING_NAME_CLICK DArc_PetAttack:LeftButton"] = "Pet attack mouseover or target"
 
-_G["BINDING_NAME_DARC_H_MOUNTING"]     = "Dismounting"
+_G["BINDING_HEADER_DARC_H_MOUNTING"]     = "Dismounting"
 _G["BINDING_NAME_DARC_SMART_DISMOUNT"] = "Dismount/Leave Vehicle"
 
 -- From some ArenaJunkies forum post, not able to relocate it atm
