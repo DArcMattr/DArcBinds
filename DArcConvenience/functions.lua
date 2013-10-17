14cf66c0af93dc57fda453a6133b8eb7f7e56f6d
David Arceneaux <david@davidthemachine.org>
2013-10-17 15:16:48 -0700

    bump TOCs; new target/focus swap macro

--- DArcConvenience/functions.lua
+++ DArcConvenience/functions.lua
@@ -4,6 +4,7 @@
 
 _G["BINDING_HEADER_DARC_H_TARGETING"]              = "Additional Targeting Functions"
 _G["BINDING_NAME_CLICK DArc_Focus:LeftButton"]     = "Focus mouseover, target, or clearfocus"
+_G["BINDING_NAME_CLICK DArc_SwapFocus:LeftButton"] = "Swap Focus with Target"
 _G["BINDING_NAME_CLICK DArc_Assist:LeftButton"]    = "Assist Focus"
 _G["BINDING_NAME_CLICK DArc_PetAttack:LeftButton"] = "Pet attack mouseover or target"
 _G["BINDING_NAME_SPELL Fishing"] = "Fishing cast"
@@ -17,6 +18,12 @@ macroFocus:SetAttribute("type1", "macro")
 macroFocus:SetAttribute("macrotext1", [[/focus [@mouseover,exists][exists]
 /stopmacro [@mouseover,exists][exists]
 /clearfocus ]])
+
+local macroFocus = CreateFrame("Button", "DArc_SwapFocus", UIParent, "SecureActionButtonTemplate")
+macroFocus:SetAttribute("type1", "macro")
+macroFocus:SetAttribute("macrotext1", [[/target [@focus]
+/focus [@lasttarget]
+]])
 
 local macroAssist = CreateFrame("Button", "DArc_Assist", UIParent, "SecureActionButtonTemplate")
 macroAssist:SetAttribute("type1", "macro")
