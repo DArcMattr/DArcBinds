630a6f7d897114a421e4c8858e18d7ae22f9caec
David Arceneaux <david@davidthemachine.org>
2018-04-13 17:29:39 -0700

    Massively streamline project structure
    * Combine all 3 directories that are in this project.
    * Convert some macro names to use index numbers for eventual loop
      conversion.
    * Label all bindings in the Addons category.

--- functions.lua
+++ functions.lua
@@ -1,4 +1,4 @@
--- Copyright (c) 2013 David Arceneaux
+-- Copyright (c) 2018 David Arceneaux
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -22,13 +22,13 @@
 -- Secure Button Code --
 ------------------------
 
-local macroMarker1 = CreateFrame("Button", "DArc_Marker1", UIParent, "SecureActionButtonTemplate")
+local macroMarker1 = CreateFrame("Button", "DArc_Marker_Square", UIParent, "SecureActionButtonTemplate")
 macroMarker1:SetAttribute("type1", "macro")
 macroMarker1:SetAttribute("macrotext1", [[/clearworldmarker 1
 /worldmarker 1
 ]])
 
-local macroMarker2 = CreateFrame("Button", "DArc_Marker2", UIParent, "SecureActionButtonTemplate")
+local macroMarker2 = CreateFrame("Button", "DArc_Marker_Triangle", UIParent, "SecureActionButtonTemplate")
 macroMarker2:SetAttribute("type1", "macro")
 macroMarker2:SetAttribute("macrotext1", [[/clearworldmarker 2
 /worldmarker 2
@@ -78,24 +78,62 @@ macroMarkerClear:SetAttribute("macrotext1", "/clearworldmarker all")
 -- Binding Labels --
 --------------------
 
-_G["BINDING_HEADER_DARC_RAID_TARGETING"]        = "Additional Raid Targeting on Mouseover or Target"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_STAR"]     = "Assign Star"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_CIRCLE"]   = "Assign Circle"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_DIAMOND"]  = "Assign Diamond"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_TRIANGLE"] = "Assign Triangle"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_MOON"]     = "Assign Moon"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_SQUARE"]   = "Assign Square"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_CROSS"]    = "Assign Cross"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_SKULL"]    = "Assign Skull"
-_G["BINDING_NAME_DARC_MARK_MOUSEOVER_CLEAR"]    = "Clear Raid Icon"
+_G["BINDING_HEADER_DARC_RAID_TARGETING"]     = "Additional Raid Targeting on Mouseover or Target"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_1"]     = "Assign {star} Star"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_2"]     = "Assign {circle} Circle"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_3"]     = "Assign {diamond} Diamond"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_4"]     = "Assign {triangle} Triangle"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_5"]     = "Assign {moon} Moon"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_6"]     = "Assign {square} Square"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_7"]     = "Assign {cross} Cross"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_8"]     = "Assign {skull} Skull"
+_G["BINDING_NAME_DARC_MARK_MOUSEOVER_CLEAR"] = "Clear Raid Icon"
 
 _G["BINDING_HEADER_DARC_MARKERS"]                    = "World Markers"
-_G["BINDING_NAME_CLICK DArc_Marker1:LeftButton"]     = "Place square Marker"
-_G["BINDING_NAME_CLICK DArc_Marker2:LeftButton"]     = "Place triangle Marker"
-_G["BINDING_NAME_CLICK DArc_Marker3:LeftButton"]     = "Place diamond Marker"
-_G["BINDING_NAME_CLICK DArc_Marker4:LeftButton"]     = "Place cross Marker"
-_G["BINDING_NAME_CLICK DArc_Marker5:LeftButton"]     = "Place star Marker"
-_G["BINDING_NAME_CLICK DArc_Marker6:LeftButton"]     = "Place circle Marker"
-_G["BINDING_NAME_CLICK DArc_Marker7:LeftButton"]     = "Place moon Marker"
-_G["BINDING_NAME_CLICK DArc_Marker8:LeftButton"]     = "Place skull Marker"
-_G["BINDING_NAME_CLICK DArc_MarkerClear:LeftButton"] = "Clear All Markers"
+_G["BINDING_NAME_CLICK DArc_Marker_1:LeftButton"]     = "Place {Square} Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_2:LeftButton"]     = "Place triangle Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_3:LeftButton"]     = "Place diamond Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_4:LeftButton"]     = "Place cross Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_5:LeftButton"]     = "Place star Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_6:LeftButton"]     = "Place circle Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_7:LeftButton"]     = "Place moon Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_8:LeftButton"]     = "Place skull Marker"
+_G["BINDING_NAME_CLICK DArc_Marker_Clear:LeftButton"] = "Clear All Markers"
+
+_G["BINDING_HEADER_DARC_H_ARCH"]     = "Archaeology"
+_G["BINDING_NAME_SPELL Survey"]      = "Survey"
+_G["BINDING_NAME_SPELL Archaeology"] = "Archaeology Frame"
+
+_G["BINDING_HEADER_DARC_H_TARGETING"]              = "Additional Targeting Functions"
+_G["BINDING_NAME_CLICK DArc_Focus:LeftButton"]     = "Focus mouseover, target, or clearfocus"
+_G["BINDING_NAME_CLICK DArc_SwapFocus:LeftButton"] = "Swap Focus with Target"
+_G["BINDING_NAME_CLICK DArc_Assist:LeftButton"]    = "Assist Focus"
+_G["BINDING_NAME_CLICK DArc_PetAttack:LeftButton"] = "Pet attack mouseover or target"
+_G["BINDING_NAME_SPELL Fishing"] = "Fishing cast"
+
+_G["BINDING_HEADER_DARC_H_MOUNTING"]     = "Dismounting"
+_G["BINDING_NAME_DARC_SMART_DISMOUNT"] = "Dismount/Leave Vehicle"
+
+-- From some ArenaJunkies forum post, not able to relocate it atm
+local macroFocus = CreateFrame("Button", "DArc_Focus", UIParent, "SecureActionButtonTemplate")
+macroFocus:SetAttribute("type1", "macro")
+macroFocus:SetAttribute("macrotext1", [[/focus [@mouseover,exists][exists]
+/stopmacro [@mouseover,exists][exists]
+/clearfocus ]])
+
+local macroFocus = CreateFrame("Button", "DArc_SwapFocus", UIParent, "SecureActionButtonTemplate")
+macroFocus:SetAttribute("type1", "macro")
+macroFocus:SetAttribute("macrotext1", [[/target focus
+/targetlasttarget
+/focus
+/targetlasttarget
+]])
+
+local macroAssist = CreateFrame("Button", "DArc_Assist", UIParent, "SecureActionButtonTemplate")
+macroAssist:SetAttribute("type1", "macro")
+macroAssist:SetAttribute("macrotext1", "/assist focus")
+
+-- @link http://www.arenajunkies.com/topic/164251-warlock-mouseoverif-exists-or-target-petattack/
+local macroPetAttack = CreateFrame("Button", "DArc_PetAttack", UIParent, "SecureActionButtonTemplate")
+macroPetAttack:SetAttribute("type1", "macro")
+macroPetAttack:SetAttribute("macrotext1", "/petattack [@mouseover,exists,nodead][nodead]")
